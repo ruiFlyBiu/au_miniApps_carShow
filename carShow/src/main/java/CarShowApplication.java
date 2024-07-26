f13536defecfd47b40a7a93af142a6465ef314d5
cjr <1601171638@qq.com>
2024-07-26 15:52:20 +0800

    feat:后端脚手架+测试commit

---
+++ carShow/src/main/java/CarShowApplication.java
@@ -0,0 +1,13 @@
+package com.au.carShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShowApplication.class, args);
+	}
+
+}
---
+++ carShow/src/test/java/com/au/carShow/CarShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.au.carShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
